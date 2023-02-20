0e8c165c877506a1bcfeffeb25a5aa2ed5ae4c79
binaryf <fr@68k.no>
2023-02-20 10:30:57 +0100

    Update tbminer_molepool_ethw.bat

--- Windows Launch Scripts/ETHW/tbminer_molepool_ethw.bat
+++ Windows Launch Scripts/ETHW/tbminer_molepool_ethw.bat
@@ -1,6 +1,6 @@
 Set ETHW_WALLET=0x59Aa6EAfb389E814441D02Ba6EF33c10726e4B16
 
 :loop
-	TBMiner --algo ethash --hostname  eu.molepool.com --port 3331 --wallet %ETHW_WALLET% --worker-name %computername%
+	TBMiner --algo ethash --hostname eu.molepool.com --port 3331 --wallet %ETHW_WALLET% --worker-name %computername%
 	timeout 10
 goto :loop
