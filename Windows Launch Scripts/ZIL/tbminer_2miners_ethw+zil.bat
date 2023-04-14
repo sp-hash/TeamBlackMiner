8a43da9ff9fb029d54ae42da3cb8a8d8ce1734f8
sp-hash <sp-hash@users.noreply.github.com>
2023-04-14 13:54:59 +0200

    Delete tbminer_2miners_ethw+zil.bat

--- Windows Launch Scripts/ZIL/tbminer_2miners_ethw+zil.bat
+++
@@ -1,7 +0,0 @@
-Set ETHW_WALLET=0xbd4c13027ec93C691cED97a7C56670bB5a3Ae7F7
-Set ZIL_WALLET=zil1xrv7nsssq3t08mjjgh26ux58pml22afdpx2sf6
-
-:loop
-    TBMiner --algo ethash+zil --hostname ethw.2miners.com --port 2020 --wallet %ETHW_WALLET% --worker-name %computername% --zil-wallet %ZIL_WALLET% --zil-hostname eu1-zil.shardpool.io --zil-port 5000
-    timeout 10
-got :loop
