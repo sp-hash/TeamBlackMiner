548a877cf040a51e6dac306e440db16e31604cfe
sp-hash <sp-hash@users.noreply.github.com>
2023-08-19 01:01:38 +0700

    fix username in script tbminer_miningpoolhub_rvn.bat

--- Windows Launch Scripts/RVN/tbminer_miningpoolhub_rvn.bat
+++ Windows Launch Scripts/RVN/tbminer_miningpoolhub_rvn.bat
@@ -1,4 +1,4 @@
-Set RVN_WALLET=R9RaKw5HNuwtbXYRXWrpUugAE8WhtMhfUk
+Set RVN_WALLET=fr
 
 :loop
     TBMiner --algo kawpow --hostname europe.kawpow-hub.miningpoolhub.com --port 20611 --wallet %RVN_WALLET% --worker-name %computername%
