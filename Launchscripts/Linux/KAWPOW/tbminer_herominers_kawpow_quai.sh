5f7f20f6ed76885f66a4f0baca0bd1f8e274371c
sp-hash <amigaguru@gmail.com>
2026-01-03 18:15:28 +0100

    Launchscript example Kawpow-Quai

---
+++ Launchscripts/Linux/KAWPOW/tbminer_herominers_kawpow_quai.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RESTART="TRUE"
+QUAI_WALLET=(wallet)
+WORKER_NAME="fr"
+
+cleanup ()
+{
+exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+while [ 1 ]
+do
+    ./TBMiner --algo kawpow --hostname de.quai.herominers.com --port 1185 --wallet $RVN_WALLET --worker-name $WORKER_NAME
+
+    if [ $RESTART == "FALSE" ]
+    then
+        exit 0
+    fi
+done
---
+++ Launchscripts/Linux/KAWPOW/tbminer_kryptex_kawpow_quai.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RESTART="TRUE"
+QUAI_WALLET=(wallet)
+WORKER_NAME="fr"
+
+cleanup ()
+{
+exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+while [ 1 ]
+do
+    ./TBMiner --algo kawpow --hostname quai-kawpow.kryptex.network --port 7043 --wallet $RVN_WALLET --worker-name $WORKER_NAME
+
+    if [ $RESTART == "FALSE" ]
+    then
+        exit 0
+    fi
+done
